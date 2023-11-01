ca9bcd92edaef7e563b2b88320f5ec9557330c08
Michael <muniu.dev@gmail.com>
2023-11-01 12:56:56 +0300

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+let names = ["a", "b", "c"];
+
+function writeCards(names, msg) {
+    let returnMessage = [];
+    for (let i = 0; i < names.length; i++) {
+        returnMessage.push(`Thank you, ${names[i]}, for the wonderful ${msg} gift!`)
+        
+    }
+    return returnMessage;
+    
+}; 
+
+
+function countDown(num) {
+    let number = Math.abs(num);
+            while (number >= 0) {
+                console.log(number); number--;
+        }
+}
+
+
